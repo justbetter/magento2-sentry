8eb487b56ed1fb4b63531fbfd5acb108c8ba81dd
Rakhal <rakhal@justbetter.nl>
2019-11-22 14:51:03 +0100

    Fixed some bugs with sentryscript

--- Block/SentryScript.php
+++ Block/SentryScript.php
@@ -7,23 +7,40 @@ use Magento\Framework\View\Element\Template;
 
 class SentryScript extends Template
 {
-    public function __construct(DataHelper $dataHelper)
-    {
+    /**
+     * SentryScript constructor.
+     * @param DataHelper $dataHelper
+     * @param Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        DataHelper $dataHelper,
+        Template\Context $context,
+        array $data = []
+    ) {
         $this->dataHelper = $dataHelper;
+
+        parent::__construct($context, $data);
     }
 
+    /**
+     * Show script tag depending on blockName.
+     *
+     * @param string $blockName
+     * @return bool
+     */
     public function canUseScriptTag($blockName)
     {
-        if (!$dataHelper->isActive() ||
-            !$dataHelper->useScriptTag() ||
-            !$dataHelper->showScriptTagInThisBlock($blockName)
-        ) {
-            return false;
-        }
-
-        return true;
+        return  !$this->dataHelper->isActive() ||
+                !$this->dataHelper->useScriptTag() ||
+                !$this->dataHelper->showScriptTagInThisBlock($blockName);
     }
 
+    /**
+     * Get the DSN of Sentry.
+     *
+     * @return string
+     */
     public function getDSN()
     {
         return $this->dataHelper->getDSN();
