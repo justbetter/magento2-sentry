5519e4ac20dc2d75c44e7a267bbc12f545144fdb
Barry vd. Heuvel <barry@fruitcake.nl>
2022-11-03 10:18:06 +0100

    Use Response plugin for finishing

--- Model/SentryPerformance.php
+++ Model/SentryPerformance.php
@@ -6,16 +6,20 @@ namespace JustBetter\Sentry\Model;
 
 // phpcs:disable Magento2.Functions.DiscouragedFunction
 
-use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\Response\Http as HttpResponse;
+use Sentry\Tracing\Transaction;
 use Sentry\Tracing\TransactionContext;
 use Sentry\Tracing\TransactionSource;
 
 class SentryPerformance
 {
+    /** @var Transaction|null */
     private $transaction;
 
-    public function startTransaction(Http $request)
+    public function startTransaction(HttpRequest $request)
     {
         $requestStartTime = $request->getServer('REQUEST_TIME_FLOAT', microtime(true));
 
@@ -50,9 +54,17 @@ class SentryPerformance
         \Sentry\SentrySdk::getCurrentHub()->setSpan($transaction);
     }
 
-    public function finishTransaction()
+    /**
+     * @param ResponseInterface|Response $response
+     */
+    public function finishTransaction(ResponseInterface $response)
     {
         if ($this->transaction) {
+
+            if ($response instanceof HttpResponse) {
+                $this->transaction->setHttpStatus($response->getStatusCode());
+            }
+
             // Finish the transaction, this submits the transaction and it's span to Sentry
             $this->transaction->finish();
 
---
+++ Plugin/SampleRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace JustBetter\Sentry\Plugin;
+
+use JustBetter\Sentry\Model\SentryPerformance;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\LayoutInterface;
+use MagentoHackathon\Toolbar\Toolbar;
+
+/**
+ * Plugin to add Toolbar to the Response add the
+ * end of the body
+ */
+class SampleRequest
+{
+    /** @var SentryPerformance */
+    private $sentryPerformance;
+
+    /** @var Http  */
+    private $request;
+
+    public function __construct(SentryPerformance $sentryPerformance, Http $request) {
+        $this->sentryPerformance = $sentryPerformance;
+        $this->request = $request;
+    }
+
+    /**
+     * Add our toolbar to the response
+     *
+     * @param ResponseInterface $response
+     */
+    public function beforeSendResponse(ResponseInterface $response)
+    {
+        $this->sentryPerformance->finishTransaction($response);
+    }
+
+}
