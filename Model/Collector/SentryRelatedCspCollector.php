87714d55c8c35566405c4800b4fc7f2a3447d31e
Benjamin Rosenberger <rosenberger@e-conomix.at>
2025-01-07 18:46:14 +0100

    :sparkles: add dynamic addition of csp whitelist depending on the configuration

---
+++ Model/Collector/SentryRelatedCspCollector.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace JustBetter\Sentry\Model\Collector;
+
+use JustBetter\Sentry\Helper\Data as DataHelper;
+use Magento\Csp\Api\PolicyCollectorInterface;
+use Magento\Csp\Model\Policy\FetchPolicy;
+
+class SentryRelatedCspCollector implements PolicyCollectorInterface
+{
+
+    public function __construct(
+        private DataHelper $dataHelper
+    ) {
+    }
+
+    public function collect(array $defaultPolicies = []): array
+    {
+        $policies = $defaultPolicies;
+        if(!$this->dataHelper->isActive()) {
+            return $policies;
+        }
+
+        if ($this->dataHelper->useScriptTag() || $this->dataHelper->useSessionReplay()) {
+            $policies[] = new FetchPolicy(
+                'script-src',
+                false,
+                ['https://browser.sentry-cdn.com']
+            );
+            $policies[] = new FetchPolicy(
+                'connect-src',
+                false,
+                ['https://*.ingest.sentry.io']
+            );
+        }
+
+        if ($this->dataHelper->useLogrocket()) {
+            $policies[] = new FetchPolicy(
+                'script-src',
+                false,
+                ['https://cdn.lr-ingest.io']
+            );
+        }
+
+        return $policies;
+    }
+}
