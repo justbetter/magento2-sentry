f636f3fe5f6cb8841d423bf81395e9b9e35cd612
Sander Jongsma <sjongsma@emico.nl>
2025-01-22 12:30:42 +0100

    fix code quality

--- Helper/Data.php
+++ Helper/Data.php
@@ -12,8 +12,6 @@ use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\App\State;
-use Magento\Framework\DB\Adapter\TableNotFoundException;
-use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Exception\RuntimeException;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Store\Model\ScopeInterface;
@@ -211,18 +209,18 @@ class Data extends AbstractHelper
             return $this->config[$storeId];
         }
 
-        if (! $this->scopeConfig->isSetFlag('sentry/environment/enabled', ScopeInterface::SCOPE_STORE)) {
+        if (!$this->scopeConfig->isSetFlag('sentry/environment/enabled', ScopeInterface::SCOPE_STORE)) {
             $this->config[$storeId]['enabled'] = $this->deploymentConfig->get('sentry') !== null;
         } else {
             $this->config[$storeId]['enabled'] = true;
         }
 
         foreach ($this->configKeys as $value) {
-            if (! $this->scopeConfig->isSetFlag('sentry/environment/enabled', ScopeInterface::SCOPE_STORE)) {
-                $this->config[$storeId][$value] = $this->deploymentConfig->get('sentry/' . $value);
+            if (!$this->scopeConfig->isSetFlag('sentry/environment/enabled', ScopeInterface::SCOPE_STORE)) {
+                $this->config[$storeId][$value] = $this->deploymentConfig->get('sentry/'.$value);
             } else {
-                $this->config[$storeId][$value] = $this->scopeConfig->getValue('sentry/environment/' . $value, ScopeInterface::SCOPE_STORE)
-                    ?? $this->deploymentConfig->get('sentry/' . $value);
+                $this->config[$storeId][$value] = $this->scopeConfig->getValue('sentry/environment/'.$value, ScopeInterface::SCOPE_STORE)
+                    ?? $this->deploymentConfig->get('sentry/'.$value);
             }
         }
 
