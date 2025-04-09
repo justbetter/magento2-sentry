1b669dab52af7df5f97f0e570b3c0260746a7773
indy koning <indy@justbetter.nl>
2025-04-09 15:13:25 +0200

    Added a logger api check for backward and forward compatibility

--- Logger/Handler/Sentry.php
+++ Logger/Handler/Sentry.php
@@ -6,46 +6,96 @@ use JustBetter\Sentry\Helper\Data;
 use JustBetter\Sentry\Model\SentryLog;
 use Magento\Framework\App\DeploymentConfig;
 use Monolog\Handler\AbstractHandler;
+use Monolog\Logger;
+use Monolog\LogRecord;
 
-class Sentry extends AbstractHandler
-{
-    /**
-     * Construct.
-     *
-     * @param Data             $sentryHelper
-     * @param SentryLog        $sentryLog
-     * @param DeploymentConfig $deploymentConfig
-     */
-    public function __construct(
-        protected Data $sentryHelper,
-        protected SentryLog $sentryLog,
-        protected DeploymentConfig $deploymentConfig,
-    ) {
-        parent::__construct();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function isHandling($record): bool
+// TODO: Remove once V2 support is dropped.
+// phpcs:disable Generic.Classes.DuplicateClassName,PSR2.Classes.ClassDeclaration
+if (Logger::API < 3) {
+    class Sentry extends AbstractHandler
     {
-        $config = $this->sentryHelper->collectModuleConfig();
-        $this->setLevel($config['log_level']);
-
-        return parent::isHandling($record) && $this->deploymentConfig->isAvailable() && $this->sentryHelper->isActive();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function handle($record): bool
-    {
-        if (!$this->isHandling($record)) {
-            return false;
+        /**
+         * Construct.
+         *
+         * @param Data             $sentryHelper
+         * @param SentryLog        $sentryLog
+         * @param DeploymentConfig $deploymentConfig
+         */
+        public function __construct(
+            protected Data $sentryHelper,
+            protected SentryLog $sentryLog,
+            protected DeploymentConfig $deploymentConfig,
+        ) {
+            parent::__construct();
         }
 
-        $this->sentryLog->send($record['message'], $record['level'], $record['context']);
+        /**
+         * @inheritDoc
+         */
+        public function isHandling(array $record): bool
+        {
+            $config = $this->sentryHelper->collectModuleConfig();
+            $this->setLevel($config['log_level']);
 
-        return false;
+            return parent::isHandling($record) && $this->deploymentConfig->isAvailable() && $this->sentryHelper->isActive();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function handle(array $record): bool
+        {
+            if (!$this->isHandling($record)) {
+                return false;
+            }
+
+            $this->sentryLog->send($record['message'], $record['level'], $record['context']);
+
+            return false;
+        }
+    }
+} else {
+    class Sentry extends AbstractHandler
+    {
+        /**
+         * Construct.
+         *
+         * @param Data             $sentryHelper
+         * @param SentryLog        $sentryLog
+         * @param DeploymentConfig $deploymentConfig
+         */
+        public function __construct(
+            protected Data $sentryHelper,
+            protected SentryLog $sentryLog,
+            protected DeploymentConfig $deploymentConfig,
+        ) {
+            parent::__construct();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function isHandling(LogRecord $record): bool
+        {
+            $config = $this->sentryHelper->collectModuleConfig();
+            $this->setLevel($config['log_level']);
+
+            return parent::isHandling($record) && $this->deploymentConfig->isAvailable() && $this->sentryHelper->isActive();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function handle(LogRecord $record): bool
+        {
+            if (!$this->isHandling($record)) {
+                return false;
+            }
+
+            $this->sentryLog->send($record['message'], $record['level'], $record['context']);
+
+            return false;
+        }
     }
 }
+// phpcs:enable Generic.Classes.DuplicateClassName,PSR2.Classes.ClassDeclaration
