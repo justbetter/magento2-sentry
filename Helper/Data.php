90bea26aea5aadb581748e4bbe835721d61c35d7
Rosanne Nicolai <nicolairj@icloud.com>
2025-06-18 13:03:03 +0200

    Adjust the check for null, to check on empty. Sometimes the value is false.

--- Helper/Data.php
+++ Helper/Data.php
@@ -254,7 +254,7 @@ class Data extends AbstractHelper
      */
     public function processConfigValue(mixed $value, array $config): mixed
     {
-        if ($value === null) {
+        if (empty($value)) {
             return null;
         }
 
