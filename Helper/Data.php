7a37fb148e1ed86ab1abea39c57bcf185f3eccdd
indy koning <indy@justbetter.nl>
2025-10-01 12:05:16 +0200

    Raise level

--- Helper/Data.php
+++ Helper/Data.php
@@ -79,7 +79,7 @@ class Data extends AbstractHelper
         ...self::NATIVE_SENTRY_CONFIG_KEYS,
         'logrocket_key'                       => ['type' => 'string'],
         'log_level'                           => ['type' => 'int'],
-        'logger_log_level'                    => ['type' => 'int', 'default' => \Monolog\Logger::DEBUG],
+        'logger_log_level'                    => ['type' => 'int', 'default' => \Monolog\Logger::WARNING],
         'errorexception_reporting'            => ['type' => 'int'], /* @deprecated by @see: error_types https://docs.sentry.io/platforms/php/configuration/options/#error_types */
         'mage_mode_development'               => ['type' => 'bool'],
         'js_sdk_version'                      => ['type' => 'string'],
