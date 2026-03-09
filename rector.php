512e5582373247f9dd37b1b79bbd8ab62c26c736
indy koning <indy@justbetter.nl>
2026-03-09 12:02:06 +0100

    Use justbetter/magento2-coding-standard

--- rector.php
+++ rector.php
@@ -2,72 +2,52 @@
 
 declare(strict_types=1);
 
-use Magento2\Rector\Src\ReplaceNewDateTimeNull;
-use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\CodingStyle\Rector\ClassConst\RemoveFinalFromConstRector;
-use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
-use Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector;
-use Rector\DeadCode\Rector\Property\RemoveUselessReadOnlyTagRector;
-use Rector\Exception\Configuration\InvalidConfigurationException;
-use Rector\Php\PhpVersionResolver\ComposerJsonPhpVersionResolver;
+use Rector\Configuration\RectorConfigBuilder;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddParamArrayDocblockBasedOnCallableNativeFuncCallRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnArrayDocblockBasedOnArrayMapRector;
-use Rector\TypeDeclaration\Rector\FunctionLike\AddParamTypeSplFixedArrayRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $magentoRector = require 'vendor/magento/magento-coding-standard/rector.php'; // phpcs:ignore Magento2.Security.IncludeFile.FoundIncludeFile
-    $magentoRector($rectorConfig);
+/** @var RectorConfigBuilder $rectorConfig */
+$rectorConfig = require 'vendor/justbetter/magento2-coding-standard/rector.php';
 
-    $rectorConfig->paths([
-        __DIR__.'/Adminhtml',
-        __DIR__.'/Block',
-        __DIR__.'/Cache',
-        __DIR__.'/Collector',
-        __DIR__.'/Config',
-        __DIR__.'/Controller',
-        __DIR__.'/etc',
-        __DIR__.'/Handler',
-        __DIR__.'/Helper',
-        __DIR__.'/Model',
-        __DIR__.'/Observer',
-        __DIR__.'/Plugin',
-        __DIR__.'/view',
-    ]);
+$rectorConfig->withPaths([
+    __DIR__.'/Adminhtml',
+    __DIR__.'/Block',
+    __DIR__.'/Cache',
+    __DIR__.'/Collector',
+    __DIR__.'/Config',
+    __DIR__.'/Controller',
+    __DIR__.'/etc',
+    __DIR__.'/Handler',
+    __DIR__.'/Helper',
+    __DIR__.'/Model',
+    __DIR__.'/Observer',
+    __DIR__.'/Plugin',
+    __DIR__.'/view',
+]);
 
-    // register a single rule
-    $rectorConfig->rules([
-        InlineConstructorDefaultToPropertyRector::class,
-        RemoveFinalFromConstRector::class,
-        RemoveEmptyClassMethodRector::class,
-        CompleteDynamicPropertiesRector::class,
-        RemoveNonExistingVarAnnotationRector::class,
-        RemoveUselessReadOnlyTagRector::class,
-        ClassPropertyAssignToConstructorPromotionRector::class,
-        AddParamArrayDocblockBasedOnCallableNativeFuncCallRector::class,
-        AddReturnArrayDocblockBasedOnArrayMapRector::class,
-        AddParamTypeSplFixedArrayRector::class,
-    ]);
+/** Define additional rules here
+ * @see: https://getrector.com/find-rule?activeRectorSetGroup=php
+ * @see: https://getrector.com/find-rule?activeRectorSetGroup=core
+ */
 
-    $rectorConfig->skip([
-        ReadOnlyPropertyRector::class,
-        ReplaceNewDateTimeNull::class,
-        RemoveExtraParametersRector::class,
-        MixedTypeRector::class,
-    ]);
+$rectorConfig->withSkip([
+    RemoveExtraParametersRector::class,
+    MixedTypeRector::class,
+]);
 
-    try {
-        $projectPhpVersion = ComposerJsonPhpVersionResolver::resolveFromCwdOrFail();
-        $phpLevelSets = \Rector\Configuration\PhpLevelSetResolver::resolveFromPhpVersion($projectPhpVersion);
-    } catch (InvalidConfigurationException) {
-        $phpLevelSets = [LevelSetList::UP_TO_PHP_81];
-    }
-    // define sets of rules
-    $rectorConfig->sets($phpLevelSets);
-};
+/** @see: https://getrector.com/documentation/levels */
+$rectorConfig->withTypeCoverageLevel(4);         // 1 is least intrusive changes, higher is more intrusive
+$rectorConfig->withCodeQualityLevel(10);          // 1 is least intrusive changes, higher is more intrusive
+$rectorConfig->withDeadCodeLevel(1);             // 1 is least intrusive changes, higher is more intrusive
+
+$rectorConfig->withPreparedSets(
+    // Only enable these when the levels above are completed and their config is removed
+    // It will automatically set their level to the highest possible.
+    // typeDeclarations: true,  // https://getrector.com/find-rule?activeRectorSetGroup=core&rectorSet=core-type-declarations
+    // codeQuality: true,       // https://getrector.com/find-rule?activeRectorSetGroup=core&rectorSet=core-code-quality
+    // deadCode: true,          // https://getrector.com/find-rule?activeRectorSetGroup=core&rectorSet=core-dead-code
+    instanceOf: false,       // https://getrector.com/find-rule?rectorSet=core-instanceof&activeRectorSetGroup=core
+    earlyReturn: false,      // https://getrector.com/find-rule?rectorSet=core-early-return&activeRectorSetGroup=core
+);
+
+return $rectorConfig;
